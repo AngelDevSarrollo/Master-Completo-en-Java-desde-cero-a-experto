5e352dfb7a9b5520ada207174b30a3d2cb17615e
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-28 13:33:28 +0200

    Primitivos numericos de punto flotante

---
+++ src/main/java/org/example/variables/PrimitivosFloat.java
@@ -0,0 +1,12 @@
+package org.example.variables;
+
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+        //2120.0f valor de punto fijo
+        //2.12e3f notacion cientifica
+        float realFloat = 2.12e3f;//2120f;
+        System.out.println("realFloat = " + realFloat);
+        
+    }  
+    
+}
