6a5ad2adaeefb34a488caf46f01b8c9b41e22c90
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-28 13:55:38 +0200

    Primitivos numericos de punto flotante y sus constantes

--- src/main/java/org/example/variables/PrimitivosFloat.java
+++ src/main/java/org/example/variables/PrimitivosFloat.java
@@ -4,8 +4,24 @@ public class PrimitivosFloat {
     public static void main(String[] args) {
         //2120.0f valor de punto fijo
         //2.12e3f notacion cientifica
-        float realFloat = 2.12e3f;//2120f;
+        float realFloat = 3.4028235e38f;
         System.out.println("realFloat = " + realFloat);
+        System.out.println("float corresponde en byte a = "+ Float.BYTES);
+        System.out.println("float corresponde en byte a = "+ Float.SIZE);
+        System.out.println("valor maximo de un byte: " + Float.MAX_VALUE);
+        System.out.println("valor minimo de un byte: " + Float.MIN_VALUE);
+        System.out.println("***************---------------***************");
+
+        double realDouble = 1.7976931348623157E308;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("double corresponde en byte a = "+ Double.BYTES);
+        System.out.println("double corresponde en byte a = "+ Double.SIZE);
+        System.out.println("valor maximo de un byte: " + Double.MAX_VALUE);
+        System.out.println("valor minimo de un byte: " + Double.MIN_VALUE);
+        System.out.println("***************---------------***************");
+
+        var varFlotatnte = 3;
+        System.out.println("varFlotatnte = " + varFlotatnte);
         
     }  
     
