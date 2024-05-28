60a0cd24a3fd19139a8c6cdaa0079959bdd855d5
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-28 13:36:05 +0200

    Primitivos numericos de punto flotante

--- src/main/java/org/example/variables/Primitivos.java
+++ src/main/java/org/example/variables/Primitivos.java
@@ -7,7 +7,7 @@ public class Primitivos {
         System.out.println("tipo byte corresponde en byte a " + Byte.BYTES);
         System.out.println("tipo byte corresponde en byte a " + Byte.SIZE);
         System.out.println("valor maximo de un byte: " + Byte.MAX_VALUE);
-        System.out.println("valor maximo de un byte: " + Byte.MIN_VALUE);
+        System.out.println("valor minimo de un byte: " + Byte.MIN_VALUE);
         System.out.println("***************---------------***************");
 
         short numeroShort = 32767;
@@ -15,7 +15,7 @@ public class Primitivos {
         System.out.println("tipo Short corresponde en Short a " + Short.BYTES);
         System.out.println("tipo Short corresponde en Short a " + Short.SIZE);
         System.out.println("valor maximo de un Short: " + Short.MAX_VALUE);
-        System.out.println("valor maximo de un Short: " + Short.MIN_VALUE);
+        System.out.println("valor minimo de un Short: " + Short.MIN_VALUE);
         System.out.println("***************---------------***************");
 
         int numeroInt = 32768;
@@ -23,7 +23,7 @@ public class Primitivos {
         System.out.println("tipo Int corresponde en Short a " + Integer.BYTES);
         System.out.println("tipo Int corresponde en Short a " + Integer.SIZE);
         System.out.println("valor maximo de un Int: " + Integer.MAX_VALUE);
-        System.out.println("valor maximo de un Int: " + Integer.MIN_VALUE);
+        System.out.println("valor minimo de un Int: " + Integer.MIN_VALUE);
         System.out.println("***************---------------***************");
 
         long numeroLong = 9223372036854775807L;
@@ -31,7 +31,7 @@ public class Primitivos {
         System.out.println("tipo Long corresponde en Short a " + Long.BYTES);
         System.out.println("tipo Long corresponde en Short a " + Long.SIZE);
         System.out.println("valor maximo de un Long: " + Long.MAX_VALUE);
-        System.out.println("valor maximo de un Long: " + Long.MIN_VALUE);
+        System.out.println("valor minimo de un Long: " + Long.MIN_VALUE);
         System.out.println("***************---------------***************");
 
         var numeroVar =1;
