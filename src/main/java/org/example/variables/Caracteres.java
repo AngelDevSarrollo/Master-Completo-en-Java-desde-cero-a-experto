f08f7bbc9508ef58300c86b9714a35c97449411f
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-28 14:45:44 +0200

    char y caracteres especiales

--- src/main/java/org/example/variables/Caracteres.java
+++ src/main/java/org/example/variables/Caracteres.java
@@ -12,6 +12,15 @@ public class Caracteres {
         System.out.println("simbolo = " + simbolo);
         System.out.println("simbolo = caracter: " + (simbolo==caracter));
 
+        char espacion = ' ';
+        char espacioUnicode = '\u0020';
+        //estos se pueden añadir directamente a un string ""Hola \t Mundo";
+        char retroceso = '\b';
+        char tabulador = '\t';
+        char nuevaLinea = '\n';
+        char retornoCarro = '\r';
+
+
         System.out.println("char corresponde en byte a = "+ Character.BYTES);
         System.out.println("char corresponde en byte a = "+ Character.SIZE);
         System.out.println("valor maximo de un byte: " + Character.MAX_VALUE);
