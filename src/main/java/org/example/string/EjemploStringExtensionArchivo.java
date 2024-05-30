a7a102cddaef73fc6e1bd5bfe66419afb859c7bb
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-30 20:09:04 +0200

    Obtener la extension de un archivo

---
+++ src/main/java/org/example/string/EjemploStringExtensionArchivo.java
@@ -0,0 +1,11 @@
+package org.example.string;
+
+public class EjemploStringExtensionArchivo {
+    public static void main(String[] args) {
+        
+        String archivo = "alguna_imagen.jpg";
+        int i = archivo.lastIndexOf(".");
+        System.out.println("archivo.length() = " + archivo.length());
+        System.out.println("archivo.substring(archivo.length()-4) = " + archivo.substring(i+1));
+    }
+}
