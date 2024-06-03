3420af33a138eeb971d511fee0e2e7b0368a3a7a
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-06-03 10:06:03 +0200

    Operadores logicos

--- src/main/java/org/example/operadores/OperadoresLogicos.java
+++ src/main/java/org/example/operadores/OperadoresLogicos.java
@@ -16,6 +16,19 @@ public class OperadoresLogicos {
         System.out.println("b2 = " + b2);
 
         boolean b3 = i == j && k >l || m == false;
+        System.out.println("b3 = " + b3);
+        // el operardo and (&) tiene preferencia asi que se examina primero
+        boolean b4 = i == j || k < l && m == true;
+        System.out.println("b4 = " + b4);
+
+        boolean b5 = true || true && false;
+        System.out.println("b5 = " + b5);
+
+        boolean b6 = true || false && false || true;// true
+        System.out.println("b6 = " + b6);
+
+        boolean b7 = ((true || false) && false) || true;//false
+        System.out.println("b7 = " + b7);
 
 
     }
