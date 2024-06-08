c13d4179c0d9c65eaa74e903dbc221983662b8ad
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-06-08 10:16:41 +0200

    For each

---
+++ src/main/java/org/example/flujosDeControl/SentenciaForEach.java
@@ -0,0 +1,16 @@
+package org.example.flujosDeControl;
+
+public class SentenciaForEach {
+    public static void main(String[] args) {
+
+        int[] numeros = {1, 3, 5, 7, 9, 11, 13, 15};
+        for(int num: numeros){
+            System.out.println("numero = " + num);
+        }
+        String[] nombres = {"Andres", "Pepe", "Maria", "Paco", "Pepa", "Lalo", "Bea", "Ana"};
+
+        for(String nombre: nombres){
+            System.out.println("nombre = " + nombre);
+        }
+    }
+}
