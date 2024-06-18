6f33f616893270a3d7b1b000879c1316b47e7e09
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-06-18 17:14:07 +0200

    Valor vs referencia

---
+++ src/main/java/org/example/valorVsReferencia/PasarPorValor.java
@@ -0,0 +1,17 @@
+package org.example.valorVsReferencia;
+
+public class PasarPorValor {
+    public static void main(String[] args) {
+        //Todas las variables primitivas se pasan por valor, y no por referencia por que no es un objeto
+         int i = 10;
+        System.out.println("iniciamos el método main con i = " + i);
+        test(i);
+        System.out.println("Finaliza el metodo main con el valor de i = " + i);
+
+    }
+    public static void test(int i){
+        System.out.println("Iniciamos el metodo test con i = " + i);
+        i = 35;
+        System.out.println("Finaliza el metodo test con i = " + i);
+    }
+}
