a11137c82d49658e61a9fbdd7423f4e499d998fd
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-07-01 10:04:17 +0200

    Java util calendar parse

---
+++ src/main/java/org/example/date/EjemploJavaUtilDateParse.java
@@ -0,0 +1,22 @@
+package org.example.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class EjemploJavaUtilDateParse {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        System.out.println("Ingrese un fecha con formato: 'yyyy-MM-dd'");
+        try {
+            Date fecha = format.parse(s.next());
+            System.out.println("fecha = " + fecha);
+            System.out.println("format = " + format.format(fecha));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
