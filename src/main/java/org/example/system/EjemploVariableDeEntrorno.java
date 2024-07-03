2caa511af05ea966a64bd28828c8e49f4927c351
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-07-03 13:01:59 +0200

    Variables de entorno

---
+++ src/main/java/org/example/system/EjemploVariableDeEntrorno.java
@@ -0,0 +1,29 @@
+package org.example.system;
+
+import java.util.Map;
+
+public class EjemploVariableDeEntrorno {
+    public static void main(String[] args) {
+
+        Map<String, String> varEnv = System.getenv();
+        System.out.println("Variable de ambiente del sistema = " + varEnv);
+
+        String username = System.getenv("USERNAME");
+        System.out.println("username = " + username);
+
+        String javaHome = System.getenv("JAVA_HOMR");
+        System.out.println("javaHome = " + javaHome);
+
+        String temDir = System.getenv("TEMP");
+        System.out.println("temDir = " + temDir);
+
+        String path = System.getenv("PATH");
+        System.out.println("path = " + path);
+
+        String path2 = varEnv.get("Path");
+        System.out.println("path2 = " + path2);
+
+        String appEnv = varEnv.get("APPLICATION_ENV");
+        System.out.println("appEnv = " + appEnv);
+    }
+}
