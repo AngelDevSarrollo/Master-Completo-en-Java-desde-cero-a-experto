d31f7fe3b57e88837f1c216d1a2d316ab0d7b398
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-07-03 19:02:42 +0200

    Ejemplo Clase Math random

---
+++ src/main/java/org/example/math/EjemploClaseMathRandom.java
@@ -0,0 +1,20 @@
+package org.example.math;
+
+public class EjemploClaseMathRandom {
+    public static void main(String[] args) {
+
+        String[] colores = {"azul","amarillo", "verde", "blanco", "negro"};
+        double random = Math.random();
+        System.out.println("random = " + random);
+        random *= colores.length;
+
+        System.out.println("random = " + random);
+        
+        random = Math.floor(random);
+        System.out.println("random = " + random);
+
+        System.out.println("colores = " + colores[(int)random]);
+
+        
+    }
+}
