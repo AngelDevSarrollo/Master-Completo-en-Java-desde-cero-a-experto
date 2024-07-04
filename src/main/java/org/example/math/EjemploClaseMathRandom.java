51fcfc49acf1d661d1e1a0c38c38ed1c201e64c3
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-07-04 10:00:46 +0200

    Ejemplo Clase Math random

--- src/main/java/org/example/math/EjemploClaseMathRandom.java
+++ src/main/java/org/example/math/EjemploClaseMathRandom.java
@@ -1,5 +1,7 @@
 package org.example.math;
 
+import java.util.Random;
+
 public class EjemploClaseMathRandom {
     public static void main(String[] args) {
 
@@ -14,6 +16,21 @@ public class EjemploClaseMathRandom {
         System.out.println("random = " + random);
 
         System.out.println("colores = " + colores[(int)random]);
+
+
+        Random randomObj =  new Random();
+        int randomInt = randomObj.nextInt(100);
+        System.out.println("randomInt = " + randomInt);
+        // ENTRE UNO Y OTRO
+        int randomInt2 = 15 + randomObj.nextInt(25 - 15);
+        System.out.println("randomInt = " + randomInt2);
+        
+        long randomLong = randomObj.nextLong();
+        System.out.println("randomLong = " + randomLong);
+
+        randomInt = randomObj.nextInt(colores.length);
+        System.out.println("randomInt = " + randomInt);
+        System.out.println("colores = " + colores[randomInt]);
 
         
     }
