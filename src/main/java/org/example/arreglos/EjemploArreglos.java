199d898915eadd58126d9a15148d29d6d4139954
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-07-20 17:44:11 +0200

    ejemplo arreglos

--- src/main/java/org/example/arreglos/EjemploArreglos.java
+++ src/main/java/org/example/arreglos/EjemploArreglos.java
@@ -1,13 +1,36 @@
 package org.example.arreglos;
 
+import java.util.Arrays;
+
 public class EjemploArreglos {
     public static void main(String[] args) {
         String[] productos = new String[7];
+        productos[0] = "Kingston Pendrive 64GB";
+        productos[1] = "Samsung Galaxy";
+        productos[2] = "Disco Duro SSD Samsung Externo";
+        productos[3] = "Asus Notebook";
+        productos[4] = "Macbook Air";
+        productos[5] = "Chromecast 4ta generacion";
+        productos[6] = "Bicicleta Oxford";
 
-        System.out.println("productos[0] = " + productos[0]);
-        System.out.println("productos[0] = " + productos[1]);
-        System.out.println("productos[0] = " + productos[2]);
-        System.out.println("productos[0] = " + productos[3]);
+        //Usando la clase Arrays y el metodo .sort ordeanmos de la a a la z
+        //Tambien funciona con numeros
+        Arrays.sort(productos);
+
+        String prod1 = productos[0];
+        String prod2 = productos[1];
+        String prod3 = productos[2];
+        String prod4 = productos[3];
+        String prod5 = productos[4];
+        String prod6 = productos[5];
+        String prod7 = productos[6];
+        System.out.println("productos[0] = " + prod1);
+        System.out.println("productos[1] = " + prod2);
+        System.out.println("productos[2] = " + prod3);
+        System.out.println("productos[3] = " + prod4);
+        System.out.println("productos[4] = " + prod5);
+        System.out.println("productos[5] = " + prod6);
+        System.out.println("productos[6] = " + prod7);
 
         int[] numeros = new int[4];
 
